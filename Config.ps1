551db8f193665eae6b932faa86aeeeb155c896fa
davidlehto <117440833+davidlehto@users.noreply.github.com>
2022-12-13 10:44:41 +0100

    Update Config.ps1

--- Config.ps1
+++ Config.ps1
@@ -16,7 +16,6 @@ Start-OSDCloud -OSVersion 'Windows 10' -OSLanguage sv-se -OSBuild 21H2 -OSEditio
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
 if ((Get-MyComputerManufacturer) -match 'Dell') {
     Write-Host  -ForegroundColor Cyan "Setting bootorder for Dell"
-    cd E:\DellBIOSProvider
     Import-Module DellBIOSProvider
     cd DellSmbios:\BootSequence
     si .\BootSequence "4"   

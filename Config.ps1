3ddc589799790d2a89fc1c3c994a5095cb332db0
davidlehto <117440833+davidlehto@users.noreply.github.com>
2023-04-03 16:09:38 +0200

    Update Config.ps1

--- Config.ps1
+++ Config.ps1
@@ -15,12 +15,12 @@ Start-OSDCloud -OSVersion 'Windows 10' -OSLanguage sv-se -OSBuild 21H2 -OSEditio
 #Anything I want can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
 if ((Get-MyComputerManufacturer) -match 'Dell') {
-    Write-Host  -ForegroundColor Cyan "Setting bootorder for Dell"
+    Write-Host  -ForegroundColor Cyan "Setting bootorder and BIOS password for Dell"
     D:\BIOS\Dell-scripts\Install-DellBiosProvider.ps1 -ModulePath D:\BIOS\DellBIOSProvider -DllPath D:\BIOS\DllFiles
     Import-Module DellBIOSProvider 
     Set-Dell1stBootdevice "uefi rst"
     Set-Item -Path DellSmbios:\Security\AdminPassword 6541779799
-    Write-Host  -ForegroundColor Cyan "Bootorder set"
+    Write-Host  -ForegroundColor Cyan "Bootorder and BIOS Password set"
 }
 
 #Restart from WinPE

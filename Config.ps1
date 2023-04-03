92dfaf204b09a726743b5337c3f8093f042906b2
davidlehto <117440833+davidlehto@users.noreply.github.com>
2023-04-03 15:34:31 +0200

    Update Config.ps1

--- Config.ps1
+++ Config.ps1
@@ -1,17 +1,3 @@
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud for Windows 10 21h2 sv-se..."
-Start-Sleep -Seconds 5
-
-#Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating OSDCloud PowerShell Module"
-Install-Module OSD -Force
-
-Write-Host  -ForegroundColor Cyan "Importing OSDCloud PowerShell Module"
-Import-Module OSD -Force
-
-#Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud with Windows 10 21h2 sv-se"
-Start-OSDCloud -OSVersion 'Windows 10' -OSLanguage sv-se -OSBuild 21H2 -OSEdition Enterprise -ZTI
-
 #Anything I want can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
 if ((Get-MyComputerManufacturer) -match 'Dell') {
@@ -19,7 +5,7 @@ if ((Get-MyComputerManufacturer) -match 'Dell') {
     D:\BIOS\Dell-scripts\Install-DellBiosProvider.ps1 -ModulePath D:\BIOS\DellBIOSProvider -DllPath D:\BIOS\DllFiles
     Import-Module DellBIOSProvider 
     Set-Item -Path DellSmbios:\Security\AdminPassword 6541779799
-    Set-Item -Path DellSmbios:\BootSequence BootList "Uefi"
+    Set-Item -Path DellSmbios:\BootSequence "hdd"
     Write-Host  -ForegroundColor Cyan "Bootorder set"
 }
 
